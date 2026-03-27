b7cc98b6d1bcfffa773f30b9daa1e8193d21c648
shekhar <shekhart442@gmail.com>
2026-03-27 21:25:28 +0530

    minor changes done

--- run-quiz.js
+++
@@ -1,3 +0,0 @@
-import { runQuiz } from './node_modules/explainit/lib/quiz.js';
-const file = process.argv[2];
-if (file) await runQuiz([file]);
--- testing.js
+++
@@ -1,6 +0,0 @@
-function toAdd(a,b) {
-    return a + b;
-}
-
-const sum = toAdd(3,6);
-console.log(sum);
